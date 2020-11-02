339b68e8e412e9a35c23501758c090a05edc41f8
majdiCh016 <majdi.chouat@esprit.tn>
2020-11-02 23:02:38 +0100

    sonar update 2

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -10,12 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EntrepriseRepository;
 
-import javax.swing.text.html.Option;
 
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
